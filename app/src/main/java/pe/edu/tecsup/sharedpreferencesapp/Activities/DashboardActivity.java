c84eb2ad378e1ed798cc2c763666b8e7daade957
Linder Hassinger <linderhassinger00@gmail.com>
2018-04-17 09:47:33 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/tecsup/sharedpreferencesapp/Activities/DashboardActivity.java
@@ -0,0 +1,52 @@
+package pe.edu.tecsup.sharedpreferencesapp.Activities;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import pe.edu.tecsup.sharedpreferencesapp.Models.User;
+import pe.edu.tecsup.sharedpreferencesapp.R;
+import pe.edu.tecsup.sharedpreferencesapp.Repository.UserRepository;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private static final String TAG = DashboardActivity.class.getSimpleName();
+
+    // SharedPreferences
+    private SharedPreferences sharedPreferences;
+
+    private TextView usernameText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        usernameText = (TextView)findViewById(R.id.fullname_text);
+
+        // init SharedPreferences
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // get username from SharedPreferences
+        String username = sharedPreferences.getString("username", null);
+        Log.d(TAG, "username: " + username);
+
+        User user = UserRepository.getUser(username);
+
+        usernameText.setText(user.getFullname());
+    }
+
+    public void callLogout(View view){
+        // remove from SharedPreferences
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        boolean success = editor.putBoolean("islogged", false).commit();
+//        boolean success = editor.clear().commit(); // not recommended
+
+        finish();
+    }
+
+}
